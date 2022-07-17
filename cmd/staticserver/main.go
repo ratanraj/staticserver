8d44c03459443c4680b642e8ede629c9b671fd55
Ratanraj Ravuri <ratanraj.r@gmail.com>
2022-07-18 01:09:26 +0530

    add content type

--- cmd/staticserver/main.go
+++ cmd/staticserver/main.go
@@ -2,34 +2,22 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
 	"github.com/ratanraj/staticserver"
 )
 
-
 func main() {
-	fp, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fp.Close()
-
-	d := json.NewDecoder(fp)
-	var conf staticserver.Config
-	err = d.Decode(&conf)
-	if err != nil {
-		log.Fatal(err)
-	}
+	conf := staticserver.LoadConfig(os.Args[1])
 
 	server := &http.Server{
-		Addr: fmt.Sprintf(":%d", conf.Port),
-		Handler: conf,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		Addr:           fmt.Sprintf(":%d", conf.Port),
+		Handler:        conf,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
--- staticserver.go
+++ staticserver.go
@@ -1,30 +1,53 @@
 package staticserver
 
-
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Response struct {
-	StatusCode int `json:"status_code"`
-	Body string `json:"body"`
+	StatusCode  int    `json:"status_code"`
+	Body        string `json:"body"`
+	ContentType string `json:"content_type"`
 }
 
 type Config struct {
-	Port int `json:"port"`
+	Port   int                 `json:"port"`
 	Routes map[string]Response `json:"routes"`
 }
 
 func (c Config) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	for path, resp := range c.Routes {
 		if path == req.URL.Path {
+			if resp.ContentType != "" {
+				w.Header().Set("Content-Type", resp.ContentType)
+			}
 			w.WriteHeader(resp.StatusCode)
 			fmt.Fprintln(w, resp.Body)
 			return
 		}
 	}
-	
+
 	w.WriteHeader(404)
 	fmt.Fprintln(w, "404 - page not found")
+}
+
+func LoadConfig(filename string) Config {
+	fp, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	d := json.NewDecoder(fp)
+	var conf Config
+	err = d.Decode(&conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return conf
 }
